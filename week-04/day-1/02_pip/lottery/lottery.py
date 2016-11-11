21172ac0f01d0cf4c7f13fd5c8e6ae08ec3565db
bereczb <balazs.berecz@gmail.com>
2016-11-11 15:39:28 +0100

    w4d1_lottery

--- week-04/day-1/02_pip/lottery/lottery.py
+++ week-04/day-1/02_pip/lottery/lottery.py
@@ -9,10 +9,10 @@ def five_most_frequent():
     lista = f.readlines()
     f.close()
 
-    lista2 = []
-    lista3 = []
+    weekly_numbers = []
+    list_of_numbers = []
     new_row = []
-    list_of_numbers = {}
+    dict_of_numbers = {}
     top5 = {1: [0, 0], 2: [0, 0], 3: [0, 0], 4: [0, 0], 5: [0, 0]}
 
     for row in lista:
@@ -23,27 +23,34 @@ def five_most_frequent():
                 semicolon += 1
                 if semicolon == 11:
                     new_row = row[charact+1:-1]
-                    lista2 = lista2 + [new_row]
-                    continue
+                    weekly_numbers = weekly_numbers + [new_row]
 
-    for numbers in lista2:
+    print('weekly_numbers in a list by weeks: ', weekly_numbers, '\n')
+
+    for numbers in weekly_numbers:
         splited_numbers = numbers.split(';')
-        lista3 += splited_numbers
+        list_of_numbers += splited_numbers
 
-    for i in range(len(lista3)):
-        lista3[i] = int(lista3[i])
+    print('list of numbers in string format (order of appearence): ', list_of_numbers,'\n')
+
+    for i in range(len(list_of_numbers)):
+        list_of_numbers[i] = int(list_of_numbers[i])
+
+    print('list of numbers in integer format (order of appearence): ', list_of_numbers, '\n')
 
     for i in range(1, 91):
-        list_of_numbers[i] = lista3.count(i)
+        dict_of_numbers[i] = list_of_numbers.count(i)
+
+    print('dictionary of numbers: ', dict_of_numbers, '\n')
 
     for i in range(1, 6):
         for j in range(1, 91):
-            if list_of_numbers[j] > top5[i][1]:
-                top5[i] = [j, list_of_numbers[j]]
-        list_of_numbers[top5[i][0]] = 0
+            if dict_of_numbers[j] > top5[i][1]:
+                top5[i] = [j, dict_of_numbers[j]]
+        dict_of_numbers[top5[i][0]] = 0
+        print('top5 step', i, ':  ', top5)
 
-    print(list_of_numbers)
-    print(top5)
+    print('\ndictionary of numbers after selection: ', dict_of_numbers, '\n')
 
     f = open('topfive.csv', 'w')
     f.write('number')
@@ -63,4 +70,4 @@ def five_most_frequent():
 
     return result
 
-five_most_frequent()
+print(five_most_frequent())
