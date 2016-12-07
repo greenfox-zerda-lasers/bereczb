78f5bb772ed3fe9920b69e270f6a9c4fe0d80662
bereczb <balazs.berecz@gmail.com>
2016-12-07 16:26:28 +0100

    w7d3_10_11

--- week-07/day-03/10.js
+++ week-07/day-03/10.js
@@ -3,3 +3,28 @@
 // create a student object
 // that has a method `addgrade`, that takes a grade from 1 to 5
 // an other method `getAverage`, that returns the average of the grades
+
+var student = {
+   grades: [],
+   addGrade: function(grade) {
+      if (grade <= 5 && grade >= 1 && grade % 1 === 0) {
+         this.grades.push(grade);
+      }
+   },
+   getAverage: function() {
+      var sumOfGrades = 0;
+      for (var i = 0; i < this.grades.length; i++){
+         sumOfGrades += this.grades[i];
+      }
+      return sumOfGrades / this.grades.length;
+   }
+};
+
+student.addGrade(1)
+student.addGrade(2)
+student.addGrade(3)
+student.addGrade(3)
+student.addGrade(1.3)
+student.addGrade('nyenye')
+console.log(student.grades)
+console.log(student.getAverage())
--- week-07/day-03/11.js
+++ week-07/day-03/11.js
@@ -6,3 +6,34 @@
 // It should have a `pop` method that returns the last element form the stack and also deletes it from it
 
 // please don`t use the built in methods
+
+function Stack() {
+   this.element = [];
+   this.size = function() {
+      return this.element.length;
+   };
+   this.push = function(element) {
+      this.element[this.element.length] = element
+   };
+   this.pop = function() {
+      var lastElement = this.element[this.element.length-1];
+      this.element.length = this.element.length - 1;
+      return lastElement;
+   }
+}
+
+var element1 = new Stack();
+
+element1.size();
+
+element1.push(1);
+element1.push(4);
+element1.push(6);
+element1.push(9);
+element1.push(51);
+element1.push(45);
+
+console.log(element1.size());
+console.log(element1.element);
+console.log(element1.pop());
+console.log(element1.element);
